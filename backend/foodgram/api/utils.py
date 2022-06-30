f4393d4e6c8b6b8cf8deb8cede7ceefc39829b6f
Galitsin Nikolay <ngalitsin@gmail.com>
2022-06-30 16:29:11 +0300

    adding gitnub actions

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 
-from api.serializers import ShortRecipeSerializer
+import api.serializers as serializers
 from recipes.models import Recipe, RecipeIngredient, RecipeTag
 
 
@@ -29,7 +29,7 @@ def add_or_remove_from_list(list_model, request, pk):
     recipe = get_object_or_404(Recipe, id=pk)
     if request.method == 'POST':
         list_model.objects.create(user=user, recipe=recipe)
-        serializer = ShortRecipeSerializer(recipe)
+        serializer = serializers.ShortRecipeSerializer(recipe)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     if request.method == 'DELETE':
         list_model.objects.filter(user=user, recipe=recipe).delete()
